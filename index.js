ceb0fc6dc3ca0b2220a65f98ec6fb0cbecfc66b3
Aniket1118 <91197235+Aniket1118@users.noreply.github.com>
2021-12-26 18:57:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//program to find the nth number of airthmetic progression
+
+
+//suppose we have  A.P = 1,2,3,4,5
+// where first term a = 1 and difference (d) = 1 and we have to find the 10th number of this AP,, so
+//we know nth no. of an AP is TN = a + (N-1) * d
+
+var a =1;
+var d = 1;
+var n = 10; //nth no. of AP which we want to find
+var c =   (a + (n-1) * d);
+console.log(c)
